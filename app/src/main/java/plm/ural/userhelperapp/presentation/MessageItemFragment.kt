9f5462b4a0ef36b72a202d1727cdb74e096bc6a1
Vadick Y <zergunchic@yandex.ru>
2022-12-26 15:29:23 +0500

    Messages on Activities

--- app/src/main/java/plm/ural/userhelperapp/presentation/MessageItemFragment.kt
+++ app/src/main/java/plm/ural/userhelperapp/presentation/MessageItemFragment.kt
@@ -2,32 +2,43 @@ package plm.ural.userhelperapp.presentation
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import plm.ural.userhelperapp.R
 
-class MessageItemActivity : AppCompatActivity() {
+class MessageItemFragment: Fragment() {
 
     private lateinit var viewModel:MessageItemViewModel
-    private lateinit var messageFld:EditText
-    private lateinit var nameFld:EditText
+    private lateinit var messageFld: EditText
+    private lateinit var nameFld: EditText
     private lateinit var saveBtn: Button
     private var messageItemId = -1
     private var screenMode = ""
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_message_item, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[MessageItemViewModel::class.java]
-        setContentView(R.layout.activity_message_item)
-        parseIntent()
-        initViews()
-        nameFld.addTextChangedListener(object: TextWatcher{
+        //setContentView(R.layout.activity_message_item)
+        parseParams()
+        initViews(view)
+        nameFld.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -40,7 +51,7 @@ class MessageItemActivity : AppCompatActivity() {
             }
 
         })
-        messageFld.addTextChangedListener(object: TextWatcher{
+        messageFld.addTextChangedListener(object: TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -55,27 +66,25 @@ class MessageItemActivity : AppCompatActivity() {
         })
         observeViewModel()
         launchRightMode()
-
-
     }
 
     private fun observeViewModel(){
-        viewModel.errorInputMessage.observe(this){
+        viewModel.errorInputMessage.observe(viewLifecycleOwner){
             val message = if(it){
                 getString(R.string.error_message)
             }else null
             messageFld.error = message
         }
 
-        viewModel.errorInputName.observe(this){
+        viewModel.errorInputName.observe(viewLifecycleOwner){
             val message = if(it){
                 getString(R.string.error_name)
             }else null
             nameFld.error = message
         }
 
-        viewModel.shouldCloseScreen.observe(this){
-            finish()
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner){
+            activity?.onBackPressed()
         }
     }
 
@@ -93,7 +102,7 @@ class MessageItemActivity : AppCompatActivity() {
     }
     private fun launchEditMode(){
         viewModel.getMessageItem(messageItemId)
-        viewModel.messageLiveData.observe(this){
+        viewModel.messageLiveData.observe(viewLifecycleOwner){
             nameFld.setText(it.userName)
             messageFld.setText(it.userMessage)
         }
@@ -102,23 +111,17 @@ class MessageItemActivity : AppCompatActivity() {
         }
     }
 
-    private fun parseIntent(){
-        if(!intent.hasExtra(EXTRA_SCREEN_MODE))
-            throw RuntimeException("param screen is absent")
-        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-        if (mode != MODE_EDIT && mode != MODE_ADD)
-            throw RuntimeException("Unknown screen mode $mode")
-        screenMode = mode
-        if(screenMode == MODE_EDIT && !intent.hasExtra(EXTRA_MESSAGE_ITEM_ID))
+    private fun parseParams(){
+        if (screenMode != MODE_EDIT && screenMode != MODE_ADD)
+            throw RuntimeException("Unknown screen mode $screenMode")
+        if(screenMode == MODE_EDIT && messageItemId == -1)
             throw RuntimeException("Param message id is not set")
-        if(screenMode == MODE_EDIT)
-            messageItemId = intent.getIntExtra(EXTRA_MESSAGE_ITEM_ID, -1)
     }
 
-    private fun initViews(){
-        messageFld = findViewById(R.id.MessageFld)
-        nameFld = findViewById(R.id.PersonName)
-        saveBtn = findViewById(R.id.saveBtn)
+    private fun initViews(view:View){
+        messageFld = view.findViewById(R.id.MessageFld)
+        nameFld = view.findViewById(R.id.PersonName)
+        saveBtn = view.findViewById(R.id.saveBtn)
     }
 
     companion object{
@@ -127,13 +130,13 @@ class MessageItemActivity : AppCompatActivity() {
         private const val MODE_EDIT = "mode_edit"
         private const val MODE_ADD = "mode_add"
 
-        fun newIntentAddMessage(context: Context):Intent{
+        fun newIntentAddMessage(context: Context): Intent {
             val intent = Intent(context, MessageItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
             return intent
         }
 
-        fun newIntentEditMessage(context: Context, messageItemId:Int):Intent{
+        fun newIntentEditMessage(context: Context, messageItemId:Int): Intent {
             val intent = Intent(context, MessageItemActivity::class.java)
             intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
             intent.putExtra(EXTRA_MESSAGE_ITEM_ID, messageItemId)
